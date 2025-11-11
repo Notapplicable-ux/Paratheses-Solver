b2e615de8c045e5710b572023fcde112f3163fdf
Notapplicable-ux <anandpopo91@gmail.com>
2025-11-11 19:24:14 +0000

    W

--- Pattern.java
+++
@@ -1,9 +0,0 @@
-//Name: Anand Raj
-//Date: 11/7/25
-//Determine if all the open parantheses in the string have mtching closed paratheses
-
-public class Solver{
-    public static boolean StringTest(String s){
-        String sub = s.substring
-    }
-}
---
+++ Solver.java
@@ -0,0 +1,17 @@
+//Name: Anand Raj
+//Date: 11/7/25
+//Determine if all the open parantheses in the string have mtching closed paratheses
+
+public class Solver{
+    public static boolean StringTest(String s){
+        String sub = s.substring(0,1);
+        String newS = s.substring(1);
+        do{
+            if(sub.equals("(")){
+                for(int i;i < s.length();i++){
+                    if(s.indexOf[i].equals("}"))
+                }
+            }
+        } 
+    }
+}
